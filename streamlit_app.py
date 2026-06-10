4b390f15edb29738ba4f0863d6f07b19dec10ee1
dgandhi10 <dgandhi10@gmail.com>
2026-06-10 16:58:27 -0500

    Update streamlit_app.py
    Moved reqeust before other imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
 import requests  
+from snowflake.snowpark.functions import col
+
 
 # Write directly to the app
 st.title("My Parents New Healthy Diner")
